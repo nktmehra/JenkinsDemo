6308bd868136e8c1d53ce2705f624c1e2ab6ba53
Ankit <nktmehra2002@gmail.com>
2020-08-29 19:57:54 +0530

    First commit

---
+++ src/main/java/com/jenkinsDemo/org/JenkinsDemo/Jenkins_1.java
@@ -0,0 +1,13 @@
+package com.jenkinsDemo.org.JenkinsDemo;
+
+import org.testng.annotations.Test;
+
+public class Jenkins_1 {
+	
+	
+	@Test
+	public void test() {
+		System.out.println("Hello World");
+	}
+
+}
